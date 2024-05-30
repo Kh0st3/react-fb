a1654a58a07cc36f0df763a5170c1c9ceefe5c7c
Ricardo Caseres Ch <ricardocc5@hotmail.com>
2024-05-29 19:08:21 -0500

    Se agrega el proyecto

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { Routes, Route } from 'react-router-dom';
+
+import Login from "./routes/Login";
+import Home from "./routes/Home";
+import Navbar from "./components/Navbar";
+
+const App = () => {
+
+  return (
+    <>
+      <Navbar />
+      <h1>App</h1>
+     <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+      </Routes>
+    </>
+  );
+};
+
+export default App;
